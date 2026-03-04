7cdbbc113c1062df5631095876e37469b8856d7d
Dushyant Sapre <35415613+dushyantsapre29@users.noreply.github.com>
2026-03-04 20:22:21 +0530

    Add solution for matrix-transpose

---
+++ matrix-transpose/matrix-transpose.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def matrix_transpose(A):
+    """
+    Return the transpose of matrix A (swap rows and columns).
+    """
+    # Write code here
+
+    arrayA = np.asarray(A)
+    m,n = arrayA.shape
+    B = np.zeros((n,m))
+    
+    for i in range(m):
+        for j in range(n):
+            B[j,i] = arrayA[i,j]
+
+    return B
